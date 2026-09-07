d2455b5a2f1d341bddc5b5b4e13f59672dc01647
Yadav Anish <an908103@gmail.com>
2026-09-07 18:37:43 +0530

    Fix: preserve HandleCors - use trustProxies instead of replacing global middleware stack

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,6 @@ use App\Http\Middleware\UseSanctumTokenFromCookie;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Middleware\TrustProxies;
 use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -17,9 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
 
-        $middleware->use([
-            TrustProxies::class,
-        ]);
+        $middleware->trustProxies(at: '*');
 
         $middleware->alias([
             'role' => RoleMiddleware::class,
