756ccba5db124abbfbc19f7e4fcc99ddd39d83ff
Yadav Anish <an908103@gmail.com>
2026-09-07 17:50:54 +0530

    app fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,10 +16,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
 
-        $middleware->append(
-            \Illuminate\Http\Middleware\HandleCors::class
-        );
-
         $middleware->alias([
             'role' => RoleMiddleware::class,
             'sanctum.cookie' => UseSanctumTokenFromCookie::class,
